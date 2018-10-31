b53ff546de6700660039bc5cf17d9ab35209105d
mariabelenamat <belen.amat29@gmail.com>
2018-10-31 00:25:51 -0300

    Se puede consultar a la BD todos los pedidos de un cliente por el DNI

--- src/test/java/daoTest/PedidoDAOTest.java
+++ src/test/java/daoTest/PedidoDAOTest.java
@@ -9,13 +9,18 @@ import backend.runner.Runner;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.List;
+
 import static backend.entities.chocolates.Forma.HUEVO;
 import static backend.entities.chocolates.TipoChocolate.BLANCO;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static sun.misc.Version.println;
 
 public class PedidoDAOTest {
 
-    private Pedido pedido;
+    private Pedido pedido1;
+    private Pedido pedido2;
     private Chocolate chocolateBlanco;
     private Chocolate chocolateConLeche;
     private PedidoDAO pedidoDAO;
@@ -32,9 +37,13 @@ public class PedidoDAOTest {
         chocolateConLeche = new Chocolate(HUEVO, TipoChocolate.CONLECHE,100);
 
         marge = new Cliente(20365694,"Marge Simpson", 12345678, "Avenida Siempreviva 742", "marge@gmail.com");
-        pedido = new Pedido(marge);
-        pedido.agregarChocolate(chocolateBlanco);
-        pedido.agregarChocolate(chocolateConLeche);
+        pedido1 = new Pedido(marge);
+        pedido2 = new Pedido(marge);
+
+        pedido1.agregarChocolate(chocolateBlanco);
+        pedido2.agregarChocolate(chocolateConLeche);
+
+
         pedidoDAO = new PedidoDAO();
     }
 
@@ -42,16 +51,32 @@ public class PedidoDAOTest {
     public void recuperoUnChocolateBlanco(){
         Runner.runInSession(() -> {
 
-            pedidoDAO.guardar(pedido);
+            pedidoDAO.guardar(pedido1);
 
-            Pedido pedidoRecuperado = pedidoDAO.recuperar(pedido.getId());
+            Pedido pedidoRecuperado = pedidoDAO.recuperar(pedido1.getId());
 
-            assertEquals(pedidoRecuperado.getChocolates().size(), pedido.getChocolates().size());
+            assertEquals(pedidoRecuperado.getChocolates().size(), pedido1.getChocolates().size());
 
             return null;
         });
     }
 
+    @Test
+    public void veoTodosLosPedidosDeUnCliente(){
+        Runner.runInSession(() -> {
+
+            pedidoDAO.guardar(pedido1);
+            pedidoDAO.guardar(pedido2);
+
+            List<Pedido> pedidosMarge = pedidoDAO.getPedidosDeCliente(marge.getDni());
+
+            assertEquals(2, pedidosMarge.size());
+            assertEquals(pedidosMarge.get(0).getDNICliente(), marge.getDni());
+            assertEquals(pedidosMarge.get(1).getDNICliente(), marge.getDni());
+
+            return null;
+        });
+    }
 
 }
 
